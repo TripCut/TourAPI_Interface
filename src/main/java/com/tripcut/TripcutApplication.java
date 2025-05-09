5f5a7ba1c7d7c3fc686751e9f0c4a638760543da
RosieOh <20172207@gm.hannam.ac.kr>
2025-05-09 11:33:14 +0900

    BUILD : Initial Commit

---
+++ src/main/java/com/tripcut/TripcutApplication.java
@@ -0,0 +1,13 @@
+package com.tripcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripcutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripcutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tripcut/TripcutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripcutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
