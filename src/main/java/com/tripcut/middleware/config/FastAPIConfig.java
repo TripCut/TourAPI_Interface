a4e04a3bb7e8f0bee9d0ebfc152e0578c2cc403e
RosieOh <dhxogns920@gmail.com>
2025-05-19 20:25:54 +0900

    FEAT : FastAPI 기반 중개서버 연결 설정 로직 추가

---
+++ src/main/java/com/tripcut/middleware/config/FastAPIConfig.java
@@ -0,0 +1,26 @@
+package com.tripcut.middleware.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class FastAPIConfig {
+    
+    @Value("${fastapi.base-url}")
+    private String fastApiBaseUrl;
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(fastApiBaseUrl)
+                .build();
+    }
+} 
