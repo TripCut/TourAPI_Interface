1a9150e9a2813edfd0a76fded6fe482eaa97731e
RosieOh <dhxogns920@gmail.com>
2025-05-19 20:11:16 +0900

    FEAT : 설명용 커스텀 어노테이션 추가

---
+++ src/main/java/com/tripcut/core/annotation/Description.java
@@ -0,0 +1,14 @@
+package com.tripcut.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Description {
+    String value();
+    String[] examples() default {};
+    String[] notes() default {};
+} 
