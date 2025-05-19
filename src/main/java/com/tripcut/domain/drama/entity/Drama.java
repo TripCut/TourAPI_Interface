9d3504b39dac93a9e0745a73b941031a8aa25c02
RosieOh <dhxogns920@gmail.com>
2025-05-19 20:21:11 +0900

    FEAT : 한국드라마 도메인 추가

---
+++ src/main/java/com/tripcut/domain/drama/entity/Drama.java
@@ -0,0 +1,35 @@
+package com.tripcut.domain.drama.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tripcut.domain.location.entity.FilmingLocation;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Drama {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String genre;
+    private String broadcastYear;
+    private String broadcastStation;
+    
+    @OneToMany(mappedBy = "drama", cascade = CascadeType.ALL)
+    private List<FilmingLocation> filmingLocations = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "drama", cascade = CascadeType.ALL)
+    private List<DramaReview> reviews = new ArrayList<>();
+} 
