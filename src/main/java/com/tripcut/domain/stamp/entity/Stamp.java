02aea29740e3c3a0a5cb75732f6021948bee9f45
RosieOh <dhxogns920@gmail.com>
2025-05-19 20:17:30 +0900

    FEAT : 스탬프 랠리 도메인 추가

---
+++ src/main/java/com/tripcut/domain/stamp/entity/Stamp.java
@@ -0,0 +1,36 @@
+package com.tripcut.domain.stamp.entity;
+
+import com.tripcut.domain.location.entity.FilmingLocation;
+import com.tripcut.domain.user.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Stamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String collectedAt;
+    private String stampImage;
+    private String stampType;
+    private String stampDescription;
+    private Integer stampPoints;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private FilmingLocation filmingLocation;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+} 
