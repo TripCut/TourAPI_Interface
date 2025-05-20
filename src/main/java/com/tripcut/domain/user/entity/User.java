67c9c2ccba7da40a6f1c3cb01bea6b50de3a2e38
RosieOh <dhxogns920@gmail.com>
2025-05-20 20:43:02 +0900

    CHORE : 사용자 엔티티 수정

--- src/main/java/com/tripcut/domain/user/entity/User.java
+++ src/main/java/com/tripcut/domain/user/entity/User.java
@@ -8,26 +8,33 @@ import com.tripcut.domain.location.entity.LocationReview;
 import com.tripcut.domain.stamp.entity.Stamp;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @Getter
+@Setter
 @NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
+    @Column(unique = true)
     private String email;
+
     private String password;
+    private String username;
     private String preferredLanguage;
     
     @ElementCollection
