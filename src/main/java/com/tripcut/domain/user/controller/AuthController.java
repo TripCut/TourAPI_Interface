5aa6f3c5745c70511a2911718037d5ee50bae0c0
RosieOh <dhxogns920@gmail.com>
2025-05-20 20:46:50 +0900

    FEAT : 토큰 기반 인증 프레젠테이션 레이어 추가

---
+++ src/main/java/com/tripcut/domain/user/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.tripcut.domain.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tripcut.domain.user.dto.LoginRequest;
+import com.tripcut.domain.user.dto.LoginResponse;
+import com.tripcut.domain.user.dto.SignupRequest;
+import com.tripcut.domain.user.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        return authService.registerUser(signupRequest);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        String token = authService.login(request.getEmail(), request.getPassword());
+        return ResponseEntity.ok(new LoginResponse(token));
+    }
+} 
