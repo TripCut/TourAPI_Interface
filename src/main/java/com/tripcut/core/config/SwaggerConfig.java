da74c3966bb038796fc1a83ea6a814b167f22e86
minseokKim6823 <alstjr6823@naver.com>
2025-06-22 19:10:30 +0900

    SETTINGS : swagger 세팅

---
+++ src/main/java/com/tripcut/core/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.tripcut.core.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TripCut API Specification",
+                description = "Specification for buds",
+                version = "v1"
+        ),
+        security = {@SecurityRequirement(name = "Bearer Authentication")}
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI OpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement()
+                        .addList("Bearer Authentication"));
+    }
+}
