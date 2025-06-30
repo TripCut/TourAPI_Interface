5bf532da475626a4c37dde926e786900a1a816d0
RosieOh <20172207@gm.hannam.ac.kr>
2025-06-30 12:25:03 +0900

    FEAT : 스탬프 비즈니스 레이어 로직 추가 #7

--- src/main/java/com/tripcut/domain/stamp/repository/StampRepository.java
+++ src/main/java/com/tripcut/domain/stamp/repository/StampRepository.java
@@ -1,4 +1,31 @@
 package com.tripcut.domain.stamp.repository;
 
-public interface StampRepository {
+import com.tripcut.domain.stamp.entity.Stamp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StampRepository extends JpaRepository<Stamp, Long> {
+    
+    List<Stamp> findByUserId(Long userId);
+    
+    List<Stamp> findByLocationId(Long locationId);
+    
+    List<Stamp> findByUserIdAndLocationId(Long userId, Long locationId);
+    
+    @Query("SELECT A FROM Stamp A WHERE A.user.id = :userId AND A.filmingLocation.drama.id = :dramaId")
+    List<Stamp> findByUserIdAndDramaId(@Param("userId") Long userId, @Param("dramaId") Long dramaId);
+    
+    @Query("SELECT COUNT(A) FROM Stamp A WHERE A.user.id = :userId AND A.filmingLocation.drama.id = :dramaId")
+    Integer getStampCountByUserIdAndDramaId(@Param("userId") Long userId, @Param("dramaId") Long dramaId);
+    
+    @Query("SELECT SUM(A.stampPoints) FROM Stamp A WHERE A.user.id = :userId")
+    Integer getTotalPointsByUserId(@Param("userId") Long userId);
+    
+    @Query("SELECT A FROM Stamp A WHERE A.user.id = :userId ORDER BY A.collectedAt DESC")
+    List<Stamp> findByUserIdOrderByCollectedAtDesc(@Param("userId") Long userId);
 }
