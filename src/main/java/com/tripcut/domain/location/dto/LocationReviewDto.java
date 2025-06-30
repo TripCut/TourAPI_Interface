bd046e15792d03414e85ee5641499f21ddf7aefc
RosieOh <20172207@gm.hannam.ac.kr>
2025-06-30 12:23:29 +0900

    FEAT : 위치 기반 리뷰 DTO 추가 #6

--- src/main/java/com/tripcut/domain/location/dto/LocationReviewDto.java
+++ src/main/java/com/tripcut/domain/location/dto/LocationReviewDto.java
@@ -1,4 +1,24 @@
 package com.tripcut.domain.location.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocationReviewDto {
+    private Long id;
+    private Long locationId;
+    private Long userId;
+    private String userUsername;
+    private String title;
+    private String content;
+    private Integer rating;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
