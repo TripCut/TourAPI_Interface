9ed50a5b8905d5077afb6c5c398396b52494c6eb
RosieOh <20172207@gm.hannam.ac.kr>
2025-06-30 12:23:57 +0900

    FEAT : 스탬프 엔티티 Setter 어노테이션 추가 #7

--- src/main/java/com/tripcut/domain/stamp/entity/Stamp.java
+++ src/main/java/com/tripcut/domain/stamp/entity/Stamp.java
@@ -11,9 +11,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Stamp {
     @Id
