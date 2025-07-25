ad20e764b2aafe7aecba396b52390977ffba4ef0
RosieOh <dhxogns920@gmail.com>
2025-07-25 15:35:35 +0900

    FEAT : 드라마 기능 요청/응답 DTO 객체 추가 #8

---
+++ src/main/java/com/tripcut/domain/drama/dto/DramaRequestDto.java
@@ -0,0 +1,14 @@
+package com.tripcut.domain.drama.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DramaRequestDto {
+    private String title;
+    private String description;
+    private String genre;
+    private String broadcastYear;
+    private String broadcastStation;
+} 
