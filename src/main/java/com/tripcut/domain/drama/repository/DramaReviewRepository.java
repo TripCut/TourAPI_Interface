db4fcc48259368b7cbc27016f14913efa56aced6
RosieOh <dhxogns920@gmail.com>
2025-07-25 15:31:17 +0900

    FEAT : 드라마 리뷰 비즈니스 레이어 추가 #8

---
+++ src/main/java/com/tripcut/domain/drama/repository/DramaReviewRepository.java
@@ -0,0 +1,7 @@
+package com.tripcut.domain.drama.repository;
+
+import com.tripcut.domain.drama.entity.DramaReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DramaReviewRepository extends JpaRepository<DramaReview, Long> {
+} 
